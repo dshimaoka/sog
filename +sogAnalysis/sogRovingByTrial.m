1e6a78c29a7aed998a4fc64492154315ef86f945
Daisuke Shimaoka <daisuke.shimaoka@monash.edu>
2024-08-30 19:00:02 +1000

    log all inputs

---
+++ +sogAnalysis/sogRovingByTrial.m
@@ -0,0 +1,249 @@
+classdef sogRovingByTrial < marmodata.mdbase
+
+    properties
+        % stimulus
+        patchDir; %direction of patch [deg]
+        radius; %radius of patches in [deg]
+        patchDirList; %list of stimulus directions
+        patchSpeed; %stimulus speed [deg/s]
+        tDur; %duration of a sequence of every trials [ms]
+        patchStart; %onset times of patch in each trial [ms]
+        patchStop; %offset times of patch in each trial [ms]
+        onFrames;
+        offFrames;
+        frameRate;
+        nSuccessivePresentations;
+       ctrl; %1: equiprobable. 0: roving 
+
+        % reward
+        rewardVol;
+        rewardRate;
+        pReward;
+        rewardTimes; %time of reward in each trial
+
+        % behavioural response
+        probOddFixation;
+        %oddFixationTime;
+        %keyPressTime; %time when a key is perssed first time in a trial [ms]
+        %reactionTime;
+    end
+
+    methods (Access = public)
+        function d = sogRovingByTrial(varargin)
+            d@marmodata.mdbase(varargin{:}); % base class constructor
+
+            d.radius = getRadius(d); %radius of patches in [deg]
+            d.patchDirList = getPatchDirList(d); %list of stimulus directions
+            d.tDur = getTDur(d); %duration of a sequence [ms]
+            d.patchStart = getPatchStart(d); %onset times of patch
+            % d.patchStop = getPatchStop(d); %offset times of patch
+            d.onFrames = getOnFrames(d);
+            d.offFrames = getOffFrames(d);
+            d.frameRate = getFrameRate(d);
+            d.patchDir = getPatchDir(d); %direction of patch [deg]
+            d.nSuccessivePresentations = getNSuccessivePresentations(d);
+            d.patchSpeed = getPatchSpeed(d);
+            d.patchFrequency = getPatchFrequency(d);
+            d.ctrl = getCtrl(d);
+
+            % % reward
+            % d.rewardVol = getRewardVol(d);
+            % d.rewardRate = getRewardRate(d);
+            % d.pReward = getPReward(d);
+            % d.rewardTimes = getRewardTimes(d);
+
+            %% behaviour
+            d.probOddFixation = getProbOddFixation(d);
+            %d.keyPressTime = getKeyPressTime(d); %time of key press after trial onset
+            %d.oddFixationTime = getOddFixationTime(d);
+            %d.reactionTime = getReactionTime(d); %time of key press after odd fixation
+
+        end
+
+        function probOddFixation = getProbOddFixation(d)
+            probOddFixation = d.meta.fixstim.probOddFixation('time',Inf).data;
+        end
+
+        function nSuccessivePresentations = getNSuccessivePresentations(d)
+            nSuccessivePresentations = unique(d.meta.cic.nSuccessivePresentations('time',inf).data);
+        end
+
+        function radius = getRadius(d)
+           radius =  unique(d.meta.patch1.sigma('time',inf).data);
+        end
+
+        function ctrl = getCtrl(d)
+            ctrl = unique(d.meta.cic.ctrl('time',inf).data);
+        end
+
+        function tDur = getTDur(d)
+            tDur = d.meta.cic.trialDuration('time',Inf).data;
+        end
+ 
+        function patchFrequency = getPatchFrequency(d)
+            patchFrequency = unique(d.meta.patch1.frequency('time', Inf).data);
+        end
+ 
+        function patchSpeed = getPatchSpeed(d)
+            patchSpeed = unique(d.meta.patch1.speed('time', Inf).data);
+        end
+        
+        function keyPressTime = getKeyPressTime(d) %FIXME only 1st key press is returned
+            %get time of key press from the onset of each trial [ms]
+
+            t0 =  d.meta.cic.firstFrame('time',Inf);
+
+            keyPressTime = cell(d.numTrials, 1);
+            for itr = 1:d.numTrials
+                % [time,trial,frame,keyTmp] = d.meta.keypress.keyIx('trial',itr);
+                [time,trial,frame,keyTmp] = d.meta.cic.pressedKey('trial',itr);
+
+                ignoreEntry = cellfun(@isempty, keyTmp);
+                keepInd = find(~ignoreEntry);
+                time = time(~ignoreEntry);
+                trial = trial(~ignoreEntry);
+                frame = frame(~ignoreEntry);
+                key = keyTmp(~ignoreEntry);
+
+                keyPressTime{itr} = 1e3*(time - t0(itr));
+                if isempty( keyPressTime{itr})
+                    keyPressTime{itr} = NaN;
+                end
+            end
+
+        end
+
+        function oddFixationTime = getOddFixationTime(d)
+
+        [time, trial, frame, data] = d.meta.fixstim.color; 
+
+        standard = cellfun(@(x)(x(1)==1), data);
+        ignoreEvents = ((frame<0) + standard) > 0;
+        time = time(~ignoreEvents);
+        trial = trial(~ignoreEvents);
+
+         oddFixationTime = cell(d.numTrials, 1);
+         for itr = 1:d.numTrials
+               t0 =  d.meta.cic.firstFrame('trial',itr);
+               theseEvents = find(trial == itr);
+               if ~isempty(time(theseEvents))
+                   oddFixationTime{itr} = 1e3*(time(trial == itr) - t0);
+               else 
+                   oddFixationTime{itr} = NaN;
+               end
+        end
+        
+        end
+
+        function [reactionTime, responseType] = getReactionTime(d)
+         oddFixationTime = d.getOddFixationTime;
+            keyPressTime = d.getKeyPressTime;
+
+            oddFixationTime_all = [];
+            keyPressTime_all = [];
+            for itr = 1:d.numTrials
+                t0 =  d.meta.cic.firstFrame('trial',itr);
+                if ~isnan(oddFixationTime{itr})
+                    oddFixationTime_all = [oddFixationTime_all oddFixationTime{itr}+1e3*t0];
+                end
+                if ~isnan(keyPressTime{itr})
+                    keyPressTime_all = [keyPressTime_all keyPressTime{itr}+1e3*t0];
+                end
+            end
+
+            reactionTime = zeros(numel(oddFixationTime_all),1);
+            for ii = 1:numel(oddFixationTime_all)
+              candidateEvents =  find(keyPressTime_all - oddFixationTime_all(ii) >0);
+              [reactionTime(ii)] = min(keyPressTime_all(candidateEvents) - oddFixationTime_all(ii));
+            end
+            miss = find(reactionTime > 2000);
+            hit = find(reactionTime < 2000);
+            
+            responseType = zeros(numel(oddFixationTime_all),1);
+            responseType(hit) = 1;
+            responseType(miss) = 0;
+        end
+
+        function frameRate = getFrameRate(d)
+             test = d.meta.cic.screen.data(1);
+             frameRate = test{1}.frameRate;
+        end
+
+        function onFrames = getOnFrames(d)
+            onFrames = d.meta.cic.onFrames.data;
+        end
+
+        function offFrames = getOffFrames(d)
+            offFrames = d.meta.cic.offFrames.data;
+        end
+
+        function patchDirList = getPatchDirList(d)
+            patchDirList = d.meta.cic.dirList.data;
+        end
+
+        function patchDir = getPatchDir(d)
+            %patch direction of every presentations of every trials
+
+            tDur_cycle = (d.onFrames + d.offFrames)*1000/d.frameRate; %[ms]
+
+            patchDir = cell(d.numTrials,1);
+            for itr = 1:d.numTrials
+                thisCond = d.condIds(itr);%d.meta.cic.condition('trial',itr).data
+
+                %tDur =
+                %d.meta.(sprintf('patch%d',thisCond)).tDur('time',inf,'trial',itr).data; %INCORRECT
+                nSuccessivePresentations = round(d.tDur(itr) / tDur_cycle);
+
+                if d.meta.cic.ctrl.data == 0
+                     patchDir{itr} = d.patchDirList(thisCond) * ones(nSuccessivePresentations, 1);
+                elseif d.meta.cic.ctrl.data == 1
+                    [time,~,frame,data_tmp]  = d.meta.(sprintf('patch%d',thisCond)).direction('trial',itr);
+                    okIdx = find(frame>=-1);
+
+                    time = time(okIdx);
+                    data_tmp = cell2mat(data_tmp);
+                    data_tmp = data_tmp(okIdx);
+
+                    mipi = median(diff(time));
+                    
+                    recorded = ones(nSuccessivePresentations,1);
+                    for istim = 1:nSuccessivePresentations-1
+                        try
+                            if time(istim+1) - time(istim) > 1.2*mipi
+                                recorded(istim+1) = 0;
+                                time = [time(1:istim) time(istim)+mipi time(istim+1:end)];
+                            end
+                        catch err
+                            recorded(istim+1) = 0;
+                        end
+                    end
+                    data = nan(nSuccessivePresentations,1);
+                    data(recorded==1) = data_tmp;
+                    redundantIdx = find(recorded==0)-1;
+                    data(recorded==0) = data(redundantIdx);
+                    patchDir{itr} = data; %error when two successive stimuli are the same directions
+                    %patchStart{itr} = time;
+                end
+            end
+        end
+
+
+        function patchStart = getPatchStart(d)
+            %all patch start times from the start of each trial
+
+            patchStart = cell(numel(d.numTrials), 1);
+            for itr = 1:d.numTrials
+                thisCond = d.condIds(itr);%d.meta.cic.condition('trial',itr).data
+
+                t0 = d.meta.cic.firstFrame('time',Inf, 'trial',itr); %= d.meta.(sprintf('patch%d',thisCond)).startTime('trial',itr);
+
+                [time,~,frame,data] = d.meta.(sprintf('patch%d',thisCond)).rsvpIsi('trial',itr);
+                data = cell2mat(data);
+                okIdx = find(data==0 & frame>=-1);
+
+                patchStart{itr} = time(okIdx) - t0;
+            end
+
+        end
+    end
+end
---
+++ obs/sogRandom.m
@@ -0,0 +1,135 @@
+function sogRandom(subject, varargin)
+%% 
+% Stream of Gratings Example
+% 
+% Shows how a single stimulus ( a grating in this case) can be shown as a 
+% rapid visual stream (RSVP) with one or more of its properties changing within a
+% trial. 
+%
+% The experiment starts with a red dot, click with a mouse to fixate, then
+% the stream of gratings will show.
+% 
+% created from sogDemo
+
+%% PARAMETER DEFINITIONS
+
+if ~exist('subject','var')
+    error('No subject name provided. Type ''help facecal'' for usage information.');
+end
+
+validateattributes(subject,{'char'},{'nonempty'},'','subject',1);
+% parse optional arguments...
+p = inputParser();
+p.KeepUnmatched = true;
+p.addParameter('subject', 'test', @(x) ischar(x));
+p.addParameter('debug',false,@(x) validateattributes(x,{'logical'},{'scalar','nonempty'}));
+% trial
+p.addParameter('tDur',5000);%number of 1 sequence [ms]
+p.addParameter('nRepPerCond',3,@(x) validateattributes(x,{'numeric'},{'scalar','nonempty'}));  % number of repeats of each condition
+% parameters for rsvp
+p.addParameter('onFrames',5);%number of frames per presentation
+p.addParameter('offFrames',2);%number of frames per presentation
+p.addParameter('ori1List',0:15:165);%0:30:330);
+p.parse(subject,varargin{:});
+args = p.Results;
+
+%% fixed parameters
+radius_init = 2;%initial fixation radius[deg] value from OcuFol and cueSaccade
+fixationDeadline = 5000; %[ms]
+fixDuration = 300; % [ms] minimum duration of fixation to initiate patch stimuli
+iti = 1000; %[ms] inter trial interval
+frequency = 0.5; %spatial frequency in cycles per visual angle in degree (not pixel) %Kapoor 2022
+
+%% Prerequisites. 
+import neurostim.*
+
+
+%% Setup CIC and the stimuli.
+c = marmolab.rigcfg('debug',args.debug, p.Unmatched); % set to false to save githash at start of each experiment!
+c.paradigm = 'sogRoving';
+c.addProperty('tDur',args.tDur); %duration of one sequence in ms
+c.addProperty('fixDuration', fixDuration);
+c.trialDuration = '@fixbhv.startTime.FIXATING+cic.tDur';
+c.screen.color.background = [0 0 0]; 
+c.iti = iti;
+
+% Create a Gabor stimulus to adadot. 
+g=stimuli.gabor(c,'patch');           
+g.color             = [0.5 0.5 0.5]; %TOBE FIXED
+g.contrast          = 0.25; %TOBE FIXED
+g.Y                 = 0; 
+g.X                 = 0;
+g.sigma             = 3;                       
+g.phaseSpeed        = 0;
+%g.orientation       = 15; 
+g.mask              ='CIRCLE';
+g.frequency         = frequency;
+g.on                =  '@fixbhv.startTime.FIXATING +cic.fixDuration'; % Start showing fixDuration [ms] after the subject starts fixating (See 'fixation' object below).
+
+% We want to show a rapid stream of gratings. Use the factorial class to
+% define these "conditions" in the stream.
+stream =design('ori');           % Define a factorial with one factor
+stream.fac1.patch.orientation = args.ori1List; % Assign orientations
+stream.randomization = 'RANDOMWITHOUTREPLACEMENT'; % Randomize
+g.addRSVP(stream,'duration', args.onFrames*1000/c.screen.frameRate, ...
+    'isi', args.offFrames*1000/c.screen.frameRate); % Tell the stimulus that it should run this stream (in every trial). 5 frames on 2 frames off.
+
+% Alternatively, you may want to stream gratings with both orientation and
+% contrast varied.
+% stream =design('ovc');           % Define a factorial with two fa0ctors
+% stream.fac1.grating.orientation = 0:30:359; % Assign orientations
+% stream.fac2.grating.contrast = [0 0.25 0.5 1]; % Contrasts including 0
+% stream.randomization = 'RANDOMWITHOUTREPLACEMENT'; % Randomize
+% g.addRSVP(stream,'log',true,'duration',5*1000/c.screen.frameRate,'isi',15*1000/c.screen.frameRate); % Tell the stimulus that it should run this stream (in every trial). 5 frames on 2 frames off.
+
+%% This is all you need for an rsvp stream. The rest is just to make it into a full experiment.
+
+% fixation point at trial start
+f = stimuli.fixation(c,'fixstim');       % Add a fixation point stimulus
+f.color             = [1 1 1];
+f.shape             = 'CIRC';           % Shape of the fixation point
+f.size              = 0.25;
+f.X                 = 0;
+f.Y                 = 0;
+f.on                = 0;                % On from the start of the trial
+f.duration = '@fixbhv.startTime.fixating+cic.fixDuration'; % Show spot briefly after fixation acquired
+
+
+
+%% Behavioral control
+
+%Make sure there is an eye tracker (or at least a virtual one)
+if isempty(c.pluginsByClass('eyetracker'))
+    e = neurostim.plugins.eyetracker(c);      %Eye tracker plugin not yet added, so use the virtual one. Mouse is used to control gaze position (click)
+    e.useMouse = true;
+end
+
+fix = behaviors.fixate(c,'fixbhv');
+fix.addProperty('radius_init',radius_init);
+fix.from            = fixationDeadline;                 % If fixation has not been achieved at this time, move to the next trial
+fix.to              = '@patch.stopTime';   % Require fixation until testGabor has been shown.
+fix.X               = 0;%'@fixstim.X';
+fix.Y               = 0;%'@fixstim.Y';
+fix.tolerance       = radius_init;
+
+
+%% Define conditions and blocks
+% We will show the stream of gratings with different contrasts in each
+% trial.
+% d=design('contrast');           % Define a factorial with one factor
+% d.fac1.grating.contrast = 0.1:0.2:1; % From 10 to 100% contrast
+% d.randomization = 'RANDOMWITHOUTREPLACEMENT';
+
+% Or (if the stream already varies contrast and orientation, we vary
+% nothing across trials).
+d=design('dummy');           % Define a factorial with one factor
+% Nothing to vary here but we need at least one condition
+d.conditions(1).patch.X = 0; % Dummy
+blck=block('block',d);                  % Define a block based on this factorial
+blck.nrRepeats  = args.nRepPerCond;                        % Each condition is repeated this many times 
+
+%% Run the experiment   
+c.cursor = 'arrow';
+% Now tell CIC how we want to run these blocks 
+c.run(blck);
+ 
