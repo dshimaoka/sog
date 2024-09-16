5358bb278087f7f11771900856296efdf681a840
Joanita_rig1 <joanita279@gmail.com>
2024-09-16 11:00:45 +1000

    g{ii}.contrast          = sprintf('@iff(isinf(patch%d.direction), 0, 1)', ii); (mauh)

--- sogBinocular.m
+++ sogBinocular.m
@@ -109,7 +109,7 @@ for ii = 1:nrConds
     elseif ii == 2
         g{ii}.color             = 0.5*[0 0 1 1];
     end
-    g{ii}.contrast          = '@iff(isinf(patch1.direction), 0, 1)';%'@iff(isinf((stimName).direction), 0, 1)';
+    g{ii}.contrast          = sprintf('@iff(isinf(patch%d.direction), 0, 1)', ii);%'@iff(isinf((stimName).direction), 0, 1)';
     g{ii}.Y                 = 0;
     g{ii}.X                 = 0;
     g{ii}.width = 2*args.radius;
